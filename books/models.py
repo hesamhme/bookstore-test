3b4b2cb23f8c0d727ca4ead6cdeaa76c144c0453
hesamhme <hesam.a.hashemi@gmail.com>
2022-11-12 01:57:29 +0330

    book models created

--- books/models.py
+++ books/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Books(models.Model):
+    STATUS_CHOICE = (
+        ('fa', 'farsi'),
+        ('en', 'english'),
+        ('etc', 'other')
+    )
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    language = models.CharField(max_length=50, choices=STATUS_CHOICE)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.title
+
