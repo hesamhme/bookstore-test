b6e094c8fa21c18179e81b73ed4e3c10c38cc545
Hesam eddin Hashemi <109273285+hesamhme@users.noreply.github.com>
2022-11-23 13:56:46 +0330

    model comment fixed.

--- books/admin.py
+++ books/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from .models import Books
+from .models import Books, Comment
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'text', 'book', 'creat_at', 'status', 'recommend',)
+
 
 admin.site.register(Books)
+admin.site.register(Comment)
--- books/models.py
+++ books/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth import get_user_model
+
+
 class Books(models.Model):
     STATUS_CHOICE = (
         ('fa', 'farsi'),
@@ -25,6 +27,12 @@ class Comment(models.Model):
     book = models.ForeignKey(Books, on_delete=models.CASCADE, related_name='comments')
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     text = models.TextField()
+    creat_at = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+    recommend = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.book} : {self.user}  : {self.creat_at}"
 
 
 
