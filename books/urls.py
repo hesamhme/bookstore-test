e4dccc87146bc1c6999f3f16efa8b067b0a6bd54
Hesam eddin Hashemi <109273285+hesamhme@users.noreply.github.com>
2022-11-23 14:59:02 +0330

    comment added for books and templet edited for show user comment.

--- books/urls.py
+++ books/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
         path('', views.BookList.as_view(), name='book_list'),
-        path('<int:pk>/detail/', views.BookDetail.as_view(), name='book_detail'),
+        path('<int:pk>/detail/', views.detail_view, name='book_detail'),
         path('<int:pk>/delete/', views.BookDelete.as_view(), name='book_delete'),
         path('update/', views.NewBook.as_view(), name='book_Update'),
         path('<int:pk>/edit/', views.EditBook.as_view(), name='book_edit'),
--- books/views.py
+++ books/views.py
@@ -1,6 +1,10 @@
 from django.views import generic
 from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404, render
+
+
 from .models import Books
+from .forms import CommentForm
 
 
 class BookList(generic.ListView):
@@ -10,9 +14,29 @@ class BookList(generic.ListView):
     context_object_name = 'books'
 
 
-class BookDetail(generic.DetailView):
-    template_name = 'books/detalview.html'
-    model = Books
+# class BookDetail(generic.DetailView):
+#     template_name = 'books/detalview.html'
+#     model = Books
+
+def detail_view(request, pk):
+    book = get_object_or_404(Books, pk=pk)
+    book_comment = book.comments.all()
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.book = book
+            new_comment.user = request.user
+            new_comment.save()
+            comment_form = CommentForm()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, 'books/detalview.html', {
+            'books': book,
+            'comment': book_comment,
+            'comment_form': comment_form,
+        })
 
 
 class BookDelete(generic.DeleteView):
